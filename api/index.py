ca0863dd9b17d597797bdef199a38f55b585fe72
ali01b <kaymak210621@gmail.com>
2025-01-26 22:05:43 +0300

    fix

--- api/index.py
+++ api/index.py
@@ -114,4 +114,5 @@ def stock_info(symbol):
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
